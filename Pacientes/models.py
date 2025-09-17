218299a3cd3c93371e5a6f6f41c0f67ee4615370
Marlon Livia. <marlon.livia@tecsup.edu.pe>
2025-09-17 17:21:04 -0500

    first commit

---
+++ Pacientes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    history = models.TextField(blank=True)
+    telef = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
